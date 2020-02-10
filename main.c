25ff4dfbe33d5f9761494ac57a663581ab48ba02
Edgar Mancilla <delkoman@gmail.com>
2020-02-10 09:12:18 -0800

    Relational operators

--- main.c
+++ main.c
@@ -1,24 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Condicional if:
+//Operadores relacionales:
 int main()
 {
-    printf("Condicion if \n");
+    printf("Operadores relacionales \n \n");
 
-    float valA, valB, valC;
-    valA = valB = 100;
+    float valA, valB;
+    valA = 5;
+    valB = 10;
 
-    printf("Primera condicion \n");
-    if(valA == valB)
-            printf("Ambos son iguales \n");
-
-
-    printf("Segunda condicion \n");
+    if(valA < valB)
+    {
+        printf("Pregunta 1: Cierto \n");
+    }
+    else
+    {
+        printf("Pregunta 1: Falso \n");
+    }
+    if(valA <= valB)
+    {
+        printf("Pregunta 2: Cierto \n");
+    }
+    else
+    {
+        printf("Pregunta 2 : Falso \n");
+    }
+    if(valA > valB)
+    {
+        printf("Pregunta 3: Cierto \n");
+    }
+    else
+    {
+        printf("Pregunta 3: Falso \n");
+    }
+    if(valA >= valB)
+    {
+        printf("Pregunta 4: Cierto \n");
+    }
+    else
+    {
+        printf("Pregunta 4: Falso \n");
+    }
     if(valA == valB)
     {
-        printf("Ambos son iguales \n");
-        valC = valA + valB;
-        printf("Ademas la suma de ambos numeros es: %f", valC);
+        printf("Pregunta 5: Cierto \n");
+    }
+    else
+    {
+        printf("Pregunta 5 : Falso \n");
+    }
+    if(valA != valB)
+    {
+        printf("Pregunta 6: Cierto \n");
+    }
+    else
+    {
+        printf("Pregunta 6: Falso \n");
     }
     return 0;
 }
