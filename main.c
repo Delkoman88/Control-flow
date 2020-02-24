6fc3d65b5934a499dbfc82228dab1fabe8875990
Edgar Mancilla <delkoman@gmail.com>
2020-02-23 22:52:19 -0800

    While loops

--- main.c
+++ main.c
@@ -1,34 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Condicional switch challenge:
+//While loops:
 int main()
 {
-   printf(" Estas en un callejon que termina en 3 caminos diferentes, elige por cual seguiras tu camino.  \n");
+  printf("Iterador while \n");
 
-   char letra;
+  int limit;
 
-    printf("\n Escribe la letra a, b o c para elegir el camino a seguir: ");
-   scanf ("%c", &letra);
+  scanf("%i", &limit);
 
-   switch (letra)
-   {
-   case 'a':
-    printf("\n El camino que elegiste te lleva directamente a una playa paradisiaca llena de todas las comodidades que jamas pudiste imaginar \n");
-    break;
+  int i = 1;
 
-   case 'b':
-    printf("\n Este camino te lleva directo a un bosque encantado lleno de peligros, pero si sobrevives te casaras con la princesa del reino \n");
-    break;
-
-   case 'c':
-    printf("\n Este camino te lleva directo a un cofre lleno de lingotes de oro, disfruta tu nueva vida! \n");
-    break;
-
-   default:
-    printf("\n Elegiste una opcion invalida, has muerto de inmediato! \n");
-    break;
-
-   }
+  while(i <= limit)
+  {
+      printf("Numero: %i \n", i);
+      i++;
+  }
 
     return 0;
 }
