55720c4f7266d92e4378126151e79be47c2fc0c0
Edgar Mancilla <delkoman@gmail.com>
2020-02-26 09:18:48 -0800

    For iterator challenge completed

--- main.c
+++ main.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Ciclo For:
+//Challenge using the for iterator to print fibonacci sequence until a user defined loop:
 int main()
 {
-    printf("Iteradores For \n");
+    printf("Serie Fibonacci \n");
 
-    int upperLim, bottomLim;
+    int a = 0;
+    int b = 1;
+    int vuelta;
 
-    printf("Imprimir en orden descendiente \n");
+//Solicitamos el limite de vueltas
+    printf("Hasta que numero de vuelta te gustaria detenerte?: ");
+    scanf("%i", &vuelta);
 
-    printf("\n Ingresar limite superior: ");
-    scanf("%i", &upperLim);
-
-    printf("\n Ingresar limite inferior: ");
-    scanf("%i", &bottomLim);
-
-    for(int i = upperLim; i >= bottomLim; i--)
+//Se realiza la sucecion de Fibonacci hasta la vuelta indicada previamente
+    for(int i = 0; i <= vuelta; i++)
 
     {
-        printf("El numero es: %i \n", i);
+        printf("El numero es: %i \n", a);
+        a = a + b;
+        b = a - b;
     }
 
 
