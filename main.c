a08323aa2e5331ca8122a922befd4ddd9f40f54b
Edgar Mancilla <delkoman@gmail.com>
2020-03-02 21:42:11 -0800

    Unidimensional arrays challenge completed

--- main.c
+++ main.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Unidimensional arrays:
+//Unidimensional array challenge:
 int main()
 {
-    printf("Arreglos unidimensionales \n");
+//Se declaran las variables
+    float Valor[5];
+    float resultado;
+ //Se piden los datos del arreglo
+    printf("Ingresa 5 numeros: \n");
+    scanf("%f%f%f%f%f", &Valor[0], &Valor[1], &Valor[2], &Valor[3], &Valor[4]);
+//Se realiza la operacion y se guarda en resultado
+    resultado = Valor[0]*Valor[1]*Valor[2]*Valor[3]*Valor[4];
 
-    int integerList[3];
-    integerList[0] = 4;
-    integerList[1] = 5;
-    integerList[2] = 6;
+//Se imprime el resultado
 
-    float floatList[5] = {23.4, 34.5, 45.6, 21.8, 56.7};
-
-    char charList[4];
-    charList[0] = 'D';
-    charList[1] = 'a';
-    charList[2] = 'n';
-    charList[3] = 'i';
-
-    printf("\n Primer entero: \t\t %i", integerList[0]);
-    printf("\n Ultimo flotante: \t\t %f", floatList[4]);
-    printf("\n Lista de caracteres: \t\t %c%c%c%c", charList[0], charList[1], charList[2], charList[3]);
+    printf("\n El resultado de todos los numeros multiplicados entre si es: %f \n", resultado);
 
     return 0;
 }
