159277bd14526f22f98bfeb32e1aa0cc739e18ad
Edgar Mancilla <delkoman@gmail.com>
2020-03-06 09:35:33 -0800

    Bidimensional arrays

--- main.c
+++ main.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Condicional if:
+//Bidimensional arrays:
 int main()
 {
-    printf("Condicion if \n");
+    printf("Arreglos bidimensionales: \n \n");
 
-    float valA, valB, valC;
-    valA = valB = 100;
+    int bidimensionalArray[4][4];
 
-    printf("Primera condicion \n");
-    if(valA == valB)
-            printf("Ambos son iguales \n");
+    bidimensionalArray [0][0] = 11;
+    bidimensionalArray [0][1] = 12;
+    bidimensionalArray [0][2] = 13;
+    bidimensionalArray [0][3] = 14;
 
+    bidimensionalArray [1][0] = 29;
+    bidimensionalArray [1][1] = 28;
+    bidimensionalArray [1][2] = 27;
+    bidimensionalArray [1][3] = 29;
 
-    printf("Segunda condicion \n");
-    if(valA == valB)
-    {
-        printf("Ambos son iguales \n");
-        valC = valA + valB;
-        printf("Ademas la suma de ambos numeros es: %f", valC);
-    }
+    bidimensionalArray [2][0] = 19;
+    bidimensionalArray [2][1] = 18;
+    bidimensionalArray [2][2] = 17;
+    bidimensionalArray [2][3] = 19;
+
+    bidimensionalArray [3][0] = 45;
+    bidimensionalArray [3][1] = 46;
+    bidimensionalArray [3][2] = 47;
+    bidimensionalArray [3][3] = 48;
+
+    printf("Valor en (0,1): %i \n", bidimensionalArray[0][0]);
+    printf("Valor en (0,2): %i \n", bidimensionalArray[0][1]);
+    printf("Valor en (0,3): %i \n", bidimensionalArray[0][2]);
+    printf("Valor en (0,4): %i \n", bidimensionalArray[0][3]);
+
     return 0;
 }
