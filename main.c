803af013480a21aaf6c92a489abae89a4fb1b83d
Edgar Mancilla <delkoman@gmail.com>
2020-03-10 08:55:35 -0700

    Bidimensional arrays challenge completed

--- main.c
+++ main.c
@@ -1,36 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Bidimensional arrays:
+//Bidimensional arrays challenge:
 int main()
 {
-    printf("Arreglos bidimensionales: \n \n");
+printf("Reto usando arreglos bidimensionales: \n \n");
+//Variables are declared
+    float suma1;
+    float suma2;
+    float suma3;
 
-    int bidimensionalArray[3][4];
+//bidimensional array
+    float bidimensionalArray[3][4] =
+    {
+        {1,1,1,1},
+        {2.5,2.5,2.5,2.5},
+        {6.5,6.5,6.5,6.5}
+    };
 
-    bidimensionalArray [0][0] = 11;
-    bidimensionalArray [0][1] = 12;
-    bidimensionalArray [0][2] = 13;
-    bidimensionalArray [0][3] = 14;
+//sums are made
+    suma1 = bidimensionalArray[0][0] + bidimensionalArray[0][1] + bidimensionalArray[0][2] + bidimensionalArray[0][3];
+    suma2 = bidimensionalArray[1][0] + bidimensionalArray[1][1] + bidimensionalArray[1][2] + bidimensionalArray[1][1];
+    suma3 = bidimensionalArray[2][0] + bidimensionalArray[2][1] + bidimensionalArray[2][2] + bidimensionalArray[2][3];
 
-    bidimensionalArray [1][0] = 29;
-    bidimensionalArray [1][1] = 28;
-    bidimensionalArray [1][2] = 27;
-    bidimensionalArray [1][3] = 29;
-
-    bidimensionalArray [2][0] = 19;
-    bidimensionalArray [2][1] = 18;
-    bidimensionalArray [2][2] = 17;
-    bidimensionalArray [2][3] = 19;
-
-    bidimensionalArray [3][0] = 45;
-    bidimensionalArray [3][1] = 46;
-    bidimensionalArray [3][2] = 47;
-    bidimensionalArray [3][3] = 48;
-
-    printf("Valor en (0,1): %i \n", bidimensionalArray[0][0]);
-    printf("Valor en (0,2): %i \n", bidimensionalArray[0][1]);
-    printf("Valor en (0,3): %i \n", bidimensionalArray[0][2]);
-    printf("Valor en (0,4): %i \n", bidimensionalArray[0][3]);
+//print results
+    printf("La sumatoria de la primer fila es: %f \n", suma1);
+    printf("La sumatoria de la primer fila es: %f \n", suma2);
+    printf("La sumatoria de la primer fila es: %f \n", suma3);
 
     return 0;
 }
