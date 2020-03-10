66e0562bc72c065b8a76fd088a3a454e36107313
Edgar Mancilla <delkoman@gmail.com>
2020-03-10 09:15:37 -0700

    one dimension arrays and iterators

--- main.c
+++ main.c
@@ -1,31 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Bidimensional arrays challenge:
+
 int main()
 {
-printf("Reto usando arreglos bidimensionales: \n \n");
-//Variables are declared
-    float suma1;
-    float suma2;
-    float suma3;
+    printf("Arreglos e iteradores: \n \n");
 
-//bidimensional array
-    float bidimensionalArray[3][4] =
+    int integerArray [11];
+
+    for(int i = 0; i < 11; i++)
     {
-        {1,1,1,1},
-        {2.5,2.5,2.5,2.5},
-        {6.5,6.5,6.5,6.5}
-    };
-
-//sums are made
-    suma1 = bidimensionalArray[0][0] + bidimensionalArray[0][1] + bidimensionalArray[0][2] + bidimensionalArray[0][3];
-    suma2 = bidimensionalArray[1][0] + bidimensionalArray[1][1] + bidimensionalArray[1][2] + bidimensionalArray[1][1];
-    suma3 = bidimensionalArray[2][0] + bidimensionalArray[2][1] + bidimensionalArray[2][2] + bidimensionalArray[2][3];
-
-//print results
-    printf("La sumatoria de la primer fila es: %f \n", suma1);
-    printf("La sumatoria de la primer fila es: %f \n", suma2);
-    printf("La sumatoria de la primer fila es: %f \n", suma3);
+        integerArray[i] = i*i;
+        printf("Valor (%i) : %i \n", i, integerArray[i]);
+    }
 
     return 0;
 }
