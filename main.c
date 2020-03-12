7c093696a6af2e1a65d477b5ed0cd42a2fd16afd
Edgar Mancilla <delkoman@gmail.com>
2020-03-12 11:40:53 -0700

    Challenge completed

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <conio.h>
+#include <time.h>
 int main()
 {
     printf("Reto arreglos bidimensionales e iteradores \n \n");
@@ -9,11 +10,15 @@ int main()
 
     for(int i = 0; i < 5; i++)
     {
-        for(int j = 0; j < 6; j++)
+        for(int j = 0; j < 5; j++)
         {
-            IntegerArray[i][j] = ((i+j) * 100 + j);
+            IntegerArray[i][j] = (6 + rand() % (11-6));
             printf("(%i, %i) : %i \n", i, j, IntegerArray[i][j]);
-
+        }
+        for(int k = 5; k < 6; k++)
+        {
+            IntegerArray[i][k] = 0;
+            printf("(%i, %i) : %i \n", i, k, IntegerArray[i][k]);
         }
     }
 
